30a3d7de9c1564ca52e4ad4c73e262036c5ab100
Isayha Raposo <72576748+isayha@users.noreply.github.com>
2021-03-16 19:25:35 -0700

    Logic fixes and code condensing.

--- custom_io.py
+++ custom_io.py
@@ -2,8 +2,24 @@
 
 import sys
 
-def get_user_int(prompt):
-    try:
-        return(int(input(prompt)))
-    except:
-        return(None)
+def get_user_int(prompt, min, max):
+    if max is None:
+        return(get_user_int_no_max(prompt, min))
+    while True:
+        try:
+            user_int = int(input(prompt))
+            if min <= user_int and user_int <= max:
+                return user_int
+        except:
+            pass
+        print("Error: Invalid Input.")
+
+def get_user_int_no_max(prompt, min):
+    while True:
+        try:
+            user_int = int(input(prompt))
+            if min <= user_int:
+                return user_int
+        except:
+            pass
+        print("Error: Invalid Input.")
--- nim_variation.py
+++ nim_variation.py
@@ -3,142 +3,115 @@
 # CPSC 482
 # Derik Dreher, Sofia Jones, Isayha Raposo
 
+# !!! = Notes for future development
+
 from custom_io import *
+from tabulate import tabulate # Requires package "tabulate" (pip install tabulate)
 
 # Main function:
 def main():
     # Game setup:
     # Get pile count:
-    valid_pile_count = False
-    while not valid_pile_count:
-        pile_count = get_user_int("Please enter the desired number of piles (> 1): ")
-        if pile_count is None or pile_count < 2:
-            print("ERROR: Invalid input.")
-        else:
-            valid_pile_count = True
+    pile_count = get_user_int("Please enter the desired number of piles (> 1): ", 2, None)
 
     piles = []
-    non_empty_pile_count = pile_count
+    non_empty_pile_indexes = list(pile_index for pile_index in range(pile_count))
+    print(non_empty_pile_indexes) # TEST ONLY
 
     # Get pile sizes:
-    for selected_pile in range(pile_count):
-        valid_pile_size = False
-        while not valid_pile_size:
-            pile_size = get_user_int("Please enter the desired size of pile " + str(selected_pile) + ": ")
-            if pile_size is None or pile_size < 1:
-                print("ERROR: Invalid input.")
-            else:
-                piles.append(pile_size)
-                valid_pile_size = True
-
-    print(piles) # TEST ONLY REMOVE LATER
+    for pile_index in range(pile_count):
+        pile_size = get_user_int("Please enter the desired number of objects in pile " + str(pile_index) + ": ", 1, None)
+        piles.append(pile_size)
 
     # Get turn order:
-    valid_selection = False
-    while not valid_selection:
-        turn_order = get_user_int("Please enter the desired turn order (0 for human-first, 1 for cpu-first): ")
-        if turn_order is None or (turn_order != 0 and turn_order != 1):
-            print("ERROR: Invalid input.")
-        else:
-            valid_selection = True
+    turn_order = get_user_int("Please enter the desired turn order (0 for human-first, 1 for cpu-first): ", 0, 1)
+    cpus_turn = bool(turn_order) # For code readability
 
     # Game logic:
     game_over = False
     while not game_over:
-        print("Piles: " + str(piles))
-        print("Index: " + str(list(index for index in range(len(piles)))))
+        print(tabulate(enumerate(piles), headers=["Pile Index:", "Objects in Pile:"]))
 
-        if turn_order == 0: # human's turn
-
+        # Human's turn logic
+        if not cpus_turn:
             # Get selected pile:
-            valid_selection = False
-            while not valid_selection:
-                selected_pile = get_user_int("Please enter the index of the pile you would like to take from: ")
-                if selected_pile is None or selected_pile < 0 or selected_pile >= pile_count:
-                    print("ERROR: Invalid input.")
-                elif piles[selected_pile] == 0:
-                    print("ERROR: Pile " + str(selected_pile) + "is empty.")
-                else:
-                    valid_selection = True
+            while True:
+                pile_index = get_user_int("Please enter the index of the pile you would like to remove objects from: ", 0, pile_count - 1)
+                if piles[pile_index] != 0:
+                    break
+                print("ERROR: Pile " + str(pile_index) + "is empty.")
 
             # Get reduction:
-            valid_selection = False
-            while not valid_selection:
-                reduction = get_user_int("Please enter the number of objects you would like to remove from pile " + str(selected_pile) + ": ")
-                if reduction is None or reduction < 1:
-                    print("ERROR: Invalid input.")
-                elif reduction > piles[selected_pile]:
-                    print("ERROR: Pile " + str(selected_pile) + "currently only contains " + str(piles(selected_pile)) + " objects.")
-                else:
-                    valid_selection = True
+            while True:
+                reduction = get_user_int("Please enter the number of objects you would like to remove from pile " + str(pile_index) + ": ", 1, piles[pile_index])
+                if reduction <= piles[pile_index]:
+                    break
+                print("ERROR: Pile " + str(pile_index) + "currently only contains " + str(piles(pile_index)) + " objects.")
 
-            piles[selected_pile] -= reduction
-            if piles[selected_pile] == 0:
-                non_empty_pile_count -= 1
+            # Reduction logic:
+            print("You remove " + str(reduction) + " object(s) from pile " + str(pile_index) + ".")
+            piles[pile_index] -= reduction
+            if piles[pile_index] == 0:
+                non_empty_pile_indexes.remove(pile_index) # !!! May have a hit on performance, could look into an alternative
         
-        if turn_order == 1: # cpu's turn
+        # CPU's turn logic
+        if cpus_turn:
+            # Calculate NIM-sum
             nim_sum = 0
             for pile_size in piles:
                 nim_sum ^= pile_size
+
+            # If the NIM-sum is 0 prior to the CPU's turn, the CPU is losing, and will remove 1 object from an arbitrary non-empty pile
+            # !!! There could be some strategy implemented here, not sure what yet
             if nim_sum == 0:
-                print("CPU is losing.") # TEST ONLY REMOVE LASTER
-                for pile in range(len(piles)):
-                    if piles[pile] != 0:
-                        print("CPU removes 1 object(s) from pile " + str(pile) + ".")
-                        piles[pile] -= 1
-                        if piles[pile] == 0:
-                            non_empty_pile_count -= 1
-                        break
+                for pile_index in non_empty_pile_indexes:
+                    print("CPU removes 1 object(s) from pile " + str(pile_index) + ".")
+                    piles[pile_index] -= 1
+                    if piles[pile_index] == 0:
+                        non_empty_pile_indexes.remove(pile_index) # !!! May have a hit on performance, could look into an alternative
+                    break
             else:
-                if non_empty_pile_count > 2:
-                    move_found = False
-                    while not move_found:
-                        for pile in range(len(piles)):
-                            nim_sum_xor_pile_size = nim_sum ^ piles[pile]
-                            if piles[pile] >= nim_sum_xor_pile_size:
-                                print("CPU removes " + str(piles[pile] - nim_sum_xor_pile_size) + " object(s) from pile " + str(pile) + ".")
-                                piles[pile] -= piles[pile] - nim_sum_xor_pile_size
-                                if piles[pile] == 0:
-                                    non_empty_pile_count -= 1
-                                move_found = True
-                                break
-                        nim_sum -= 1
-                        
+                # If the NIM-sum is NOT 0 prior to the CPU's turn, the CPU can win as long as the NIM-sum is 0 at the END of it's turn
+                if len(non_empty_pile_indexes) > 2:
+                    for pile_index in non_empty_pile_indexes:
+                        nim_sum_xor_pile_size = nim_sum ^ piles[pile_index]
+                        if piles[pile_index] >= nim_sum_xor_pile_size:
+                            print("CPU removes " + str(piles[pile_index] - nim_sum_xor_pile_size) + " object(s) from pile " + str(pile_index) + ".")
+                            piles[pile_index] -= piles[pile_index] - nim_sum_xor_pile_size
+                            if piles[pile_index] == 0:
+                                non_empty_pile_indexes.remove(pile_index) # !!! May have a hit on performance, could look into an alternative
+                            break
                 else:
-                    if non_empty_pile_count == 1:
-                        for pile in range(len(piles)):
-                            if piles[pile] > 0:
-                                print("CPU removes " + str(piles[pile] - 1) + " objects from pile " + str(pile) + ".")
-                                piles[pile] = 1
-                                break
+                    if len(non_empty_pile_indexes) == 1:
+                        for pile_index in non_empty_pile_indexes:
+                            print("CPU removes " + str(piles[pile_index] - 1) + " object(s) from pile " + str(pile_index) + ".")
+                            piles[pile_index] = 1
+                            break
                     else:
-                        if any(pile_size == 1 for pile, pile_size in enumerate(piles)):
-                            for pile in range(len(piles)):
-                                if piles[pile] > 1:
-                                    print("CPU removes " + str(piles[pile]) + " object(s) from pile " + str(pile) + ".")
-                                    piles[pile] = 0
-                                    non_empty_pile_count += 1
+                        if any(piles[pile_index] == 1 for pile_index in non_empty_pile_indexes):
+                            for pile_index in non_empty_pile_indexes:
+                                if piles[pile_index] > 1:
+                                    print("CPU removes " + str(piles[pile_index]) + " object(s) from pile " + str(pile_index) + ".")
+                                    piles[pile_index] = 0
+                                    non_empty_pile_indexes.remove(pile_index) # !!! May have a hit on performance, could look into an alternative
                                     break
                         else:
-                            smallest_pile = min(pile for pile in piles if pile > 0)
-                            for pile in range(len(piles)):
-                                if piles[pile] != smallest_pile:
-                                    print("CPU removes " + str(piles[pile] - smallest_pile) + " object(s) from pile " + str(pile) + ".")
-                                    piles[pile] = smallest_pile
+                            smallest_pile_size = min(pile_size for pile_size in piles if pile_size > 0)
+                            for pile_index in non_empty_pile_indexes:
+                                if piles[pile_index] != smallest_pile_size:
+                                    print("CPU removes " + str(piles[pile_index] - (piles[pile_index] - smallest_pile_size)) + " object(s) from pile " + str(pile_index) + ".")
+                                    piles[pile_index] = smallest_pile_size
                                     break
-
-
-                    
         
         # Check if game is over:
-        if all(pile_size == 0 for pile_size in piles): # Check if all piles are empty
+        if not non_empty_pile_indexes: # Check if all piles are empty
             print("All piles are empty.")
             game_over = True
-        # Add logic for Chen's constraints HERE as an ELIF
+        # !!! Add logic for Chen's constraints HERE as an ELIF
         else:
-            turn_order = not(turn_order) # Rotate turns
+            cpus_turn = not(cpus_turn) # Rotate turns
 
-    if turn_order == 0:
+    if not cpus_turn:
         print("CPU wins!")
     else:
         print("Human wins!")
