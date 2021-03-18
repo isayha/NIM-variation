eaa88e0cdd6b8554c1b3f9bbd2cfaa7f6ebb2c61
Isayha Raposo <72576748+isayha@users.noreply.github.com>
2021-03-17 18:32:41 -0700

    Fixed issue on need_checking branch and further reduced repeats in code.

--- nim_variation.py
+++ nim_variation.py
@@ -8,6 +8,71 @@
 from custom_io import *
 from tabulate import tabulate # Requires package "tabulate" (pip install tabulate)
 
+def print_reduction(player, reduction, pile_index):
+    print(player + " removes " + str(reduction) + " object(s) from pile " + str(pile_index) + ".")
+
+def human_plays(pile_count, piles, non_empty_pile_indexes):
+    # Get selected pile:
+    while True:
+        pile_index = get_user_int("Please enter the index of the pile you would like to remove objects from: ", 0, pile_count - 1)
+        if piles[pile_index] != 0:
+            break
+        print("ERROR: Pile " + str(pile_index) + " is empty.")
+
+    # Get reduction:
+    while True:
+        reduction = get_user_int("Please enter the number of objects you would like to remove from pile " + str(pile_index) + ": ", 1, piles[pile_index])
+        if reduction <= piles[pile_index]:
+            break
+        print("ERROR: Pile " + str(pile_index) + " currently only contains " + str(piles(pile_index)) + " objects.")
+
+    return(reduction, pile_index)
+
+def cpu_plays(pile_count, piles, non_empty_pile_indexes):
+    # Calculate NIM-sum
+    nim_sum = 0
+    for pile_size in piles:
+        nim_sum ^= pile_size
+
+    # If the NIM-sum is 0 prior to the CPU's turn, the CPU is losing, and will remove 1 object from an arbitrary non-empty pile
+    # !!! There could be some strategy implemented here, not sure what yet
+    # !!! e.g: Increasing the NIM-sum as much as possible may be a good strategy (would have to check math)
+    if nim_sum == 0:
+        for pile_index in non_empty_pile_indexes:
+            return(1, pile_index)
+    # If the NIM-sum is NOT 0 prior to the CPU's turn, the CPU can win as long as the NIM-sum is 0 at the END of it's turn
+    else:
+        # Strategy for states in which the non-empty pile count is greater than 2 (Keep the NIM-sum equal to 0)
+        if len(non_empty_pile_indexes) > 2:
+            for pile_index in non_empty_pile_indexes:
+                nim_sum_xor_pile_size = nim_sum ^ piles[pile_index]
+                if piles[pile_index] >= nim_sum_xor_pile_size:
+                    reduction = piles[pile_index] - nim_sum_xor_pile_size
+                    return(reduction, pile_index)
+        # Strategy for states in which the non-empty pile count is less than or equal to 2
+        else:
+            # If the non-empty pile count is equal to 1, remove all but 1 object from the single non-empty pile, forcing the human player's hand
+            if len(non_empty_pile_indexes) == 1:
+                for pile_index in non_empty_pile_indexes:
+                    reduction = piles[pile_index] - 1
+                    return(reduction, pile_index)
+            # If the non-empty pile count is equal to 2...
+            else:
+                # ...and 1 of the 2 non-empty piles contains just 1 object, remove all of the objects from the OPPOSITE pile, forcing the human player's hand
+                if any(piles[pile_index] == 1 for pile_index in non_empty_pile_indexes):
+                    for pile_index in non_empty_pile_indexes:
+                        if piles[pile_index] > 1:
+                            reduction = piles[pile_index]
+                            return(reduction, pile_index)
+                else:
+                    # ...and none of the 2 non-empty piles contains just 1 object, ensure the 2 non-empty piles have an equal number of objects
+                    # In other words, ensure the NIM-sum is 0 at the end of the turn
+                    smallest_pile_size = min(pile_size for pile_size in piles if pile_size > 0)
+                    for pile_index in non_empty_pile_indexes:
+                        if piles[pile_index] != smallest_pile_size:
+                            reduction = piles[pile_index] - (piles[pile_index] - smallest_pile_size)
+                            return(reduction, pile_index)
+
 # Main function:
 def main():
     # Game setup:
@@ -16,7 +81,6 @@ def main():
 
     piles = []
     non_empty_pile_indexes = list(pile_index for pile_index in range(pile_count))
-    print(non_empty_pile_indexes) # TEST ONLY
 
     # Get pile sizes:
     for pile_index in range(pile_count):
@@ -30,79 +94,25 @@ def main():
     # Game logic:
     game_over = False
     while not game_over:
+        # Print status of piles
         print(tabulate(enumerate(piles), headers=["Pile Index:", "Objects in Pile:"]))
 
         # Human's turn logic
         if not cpus_turn:
-            # Get selected pile:
-            while True:
-                pile_index = get_user_int("Please enter the index of the pile you would like to remove objects from: ", 0, pile_count - 1)
-                if piles[pile_index] != 0:
-                    break
-                print("ERROR: Pile " + str(pile_index) + "is empty.")
+            # Get reduction/pile index:
+            reduction, pile_index = human_plays(pile_count, piles, non_empty_pile_indexes) # !!! If we don't have to pass any variables by adjusting scope, that would be ideal
+            print_reduction("Human", reduction, pile_index)
 
-            # Get reduction:
-            while True:
-                reduction = get_user_int("Please enter the number of objects you would like to remove from pile " + str(pile_index) + ": ", 1, piles[pile_index])
-                if reduction <= piles[pile_index]:
-                    break
-                print("ERROR: Pile " + str(pile_index) + "currently only contains " + str(piles(pile_index)) + " objects.")
-
-            # Reduction logic:
-            print("You remove " + str(reduction) + " object(s) from pile " + str(pile_index) + ".")
-            piles[pile_index] -= reduction
-            if piles[pile_index] == 0:
-                non_empty_pile_indexes.remove(pile_index) # !!! May have a hit on performance, could look into an alternative
-        
         # CPU's turn logic
-        if cpus_turn:
-            # Calculate NIM-sum
-            nim_sum = 0
-            for pile_size in piles:
-                nim_sum ^= pile_size
+        else:
+            reduction, pile_index = cpu_plays(pile_count, piles, non_empty_pile_indexes) # !!! If we don't have to pass any variables by adjusting scope, that would be ideal
+            print_reduction("CPU", reduction, pile_index)
 
-            # If the NIM-sum is 0 prior to the CPU's turn, the CPU is losing, and will remove 1 object from an arbitrary non-empty pile
-            # !!! There could be some strategy implemented here, not sure what yet
-            if nim_sum == 0:
-                for pile_index in non_empty_pile_indexes:
-                    print("CPU removes 1 object(s) from pile " + str(pile_index) + ".")
-                    piles[pile_index] -= 1
-                    if piles[pile_index] == 0:
-                        non_empty_pile_indexes.remove(pile_index) # !!! May have a hit on performance, could look into an alternative
-                    break
-            else:
-                # If the NIM-sum is NOT 0 prior to the CPU's turn, the CPU can win as long as the NIM-sum is 0 at the END of it's turn
-                if len(non_empty_pile_indexes) > 2:
-                    for pile_index in non_empty_pile_indexes:
-                        nim_sum_xor_pile_size = nim_sum ^ piles[pile_index]
-                        if piles[pile_index] >= nim_sum_xor_pile_size:
-                            print("CPU removes " + str(piles[pile_index] - nim_sum_xor_pile_size) + " object(s) from pile " + str(pile_index) + ".")
-                            piles[pile_index] -= piles[pile_index] - nim_sum_xor_pile_size
-                            if piles[pile_index] == 0:
-                                non_empty_pile_indexes.remove(pile_index) # !!! May have a hit on performance, could look into an alternative
-                            break
-                else:
-                    if len(non_empty_pile_indexes) == 1:
-                        for pile_index in non_empty_pile_indexes:
-                            print("CPU removes " + str(piles[pile_index] - 1) + " object(s) from pile " + str(pile_index) + ".")
-                            piles[pile_index] = 1
-                            break
-                    else:
-                        if any(piles[pile_index] == 1 for pile_index in non_empty_pile_indexes):
-                            for pile_index in non_empty_pile_indexes:
-                                if piles[pile_index] > 1:
-                                    print("CPU removes " + str(piles[pile_index]) + " object(s) from pile " + str(pile_index) + ".")
-                                    piles[pile_index] = 0
-                                    non_empty_pile_indexes.remove(pile_index) # !!! May have a hit on performance, could look into an alternative
-                                    break
-                        else:
-                            smallest_pile_size = min(pile_size for pile_size in piles if pile_size > 0)
-                            for pile_index in non_empty_pile_indexes:
-                                if piles[pile_index] != smallest_pile_size:
-                                    print("CPU removes " + str(piles[pile_index] - (piles[pile_index] - smallest_pile_size)) + " object(s) from pile " + str(pile_index) + ".")
-                                    piles[pile_index] = smallest_pile_size
-                                    break
-        
+        # Reduction logic:
+        piles[pile_index] -= reduction
+        if piles[pile_index] == 0:
+            non_empty_pile_indexes.remove(pile_index) # !!! May have a hit on performance, could look into an alternative
+
         # Check if game is over:
         if not non_empty_pile_indexes: # Check if all piles are empty
             print("All piles are empty.")
