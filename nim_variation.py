f5193abe6f8ca13827485af9ad22214447903c2f
Isayha Raposo <72576748+isayha@users.noreply.github.com>
2021-03-30 01:24:52 -0700

    Added constraint avoidance, attempts to lock opponent into constraint, not perfected.

--- nim_variation.py
+++ nim_variation.py
@@ -28,7 +28,11 @@ def human_plays(pile_count, piles, non_empty_pile_indexes):
 
     return(reduction, pile_index)
 
+blacklist = {}
+blacklist.update({frozenset([1,2,3]) : False}) # False is an arbitrary value
 def cpu_plays(pile_count, piles, non_empty_pile_indexes):
+    problematic_state = None
+
     # Calculate NIM-sum
     nim_sum = 0
     for pile_size in piles:
@@ -48,14 +52,28 @@ def cpu_plays(pile_count, piles, non_empty_pile_indexes):
                 nim_sum_xor_pile_size = nim_sum ^ piles[pile_index]
                 if piles[pile_index] >= nim_sum_xor_pile_size:
                     reduction = piles[pile_index] - nim_sum_xor_pile_size
-                    return(reduction, pile_index)
+
+                    temp_piles = piles.copy()
+                    temp_piles[pile_index] -= reduction
+                    if frozenset(temp_piles) in blacklist:
+                        problematic_state = temp_piles
+                        continue
+                    else:
+                        return(reduction, pile_index)
         # Strategy for states in which the non-empty pile count is less than or equal to 2
         else:
             # If the non-empty pile count is equal to 1, remove all but 1 object from the single non-empty pile, forcing the human player's hand
             if len(non_empty_pile_indexes) == 1:
                 for pile_index in non_empty_pile_indexes:
                     reduction = piles[pile_index] - 1
-                    return(reduction, pile_index)
+
+                    temp_piles = piles.copy()
+                    temp_piles[pile_index] -= reduction
+                    if frozenset(temp_piles) in blacklist:
+                        problematic_state = temp_piles
+                        break
+                    else:
+                        return(reduction, pile_index)
             # If the non-empty pile count is equal to 2...
             else:
                 # ...and 1 of the 2 non-empty piles contains just 1 object, remove all of the objects from the OPPOSITE pile, forcing the human player's hand
@@ -63,7 +81,14 @@ def cpu_plays(pile_count, piles, non_empty_pile_indexes):
                     for pile_index in non_empty_pile_indexes:
                         if piles[pile_index] > 1:
                             reduction = piles[pile_index]
-                            return(reduction, pile_index)
+                            
+                            temp_piles = piles.copy()
+                            temp_piles[pile_index] -= reduction
+                            if frozenset(temp_piles) in blacklist:
+                                problematic_state = temp_piles
+                                break
+                            else:
+                                return(reduction, pile_index)
                 else:
                     # ...and none of the 2 non-empty piles contains just 1 object, ensure the 2 non-empty piles have an equal number of objects
                     # In other words, ensure the NIM-sum is 0 at the end of the turn
@@ -71,7 +96,50 @@ def cpu_plays(pile_count, piles, non_empty_pile_indexes):
                     for pile_index in non_empty_pile_indexes:
                         if piles[pile_index] != smallest_pile_size:
                             reduction = piles[pile_index] - smallest_pile_size
-                            return(reduction, pile_index)
+
+                            temp_piles = piles.copy()
+                            temp_piles[pile_index] -= reduction
+                            if frozenset(temp_piles) in blacklist:
+                                problematic_state = temp_piles
+                                break
+                            else:
+                                return(reduction, pile_index)
+
+        # Logic for cases in which the only move with outcome NIM-sum = 0 leads to a blacklisted (immediate loss) state
+        # Ideally a move is picked so that the other player is forced into the same situation on their next turn
+        print("No move leading to a winning state found... Checking other possibilities.")
+
+        # The best move is that which best forces the other player into the same situation on their next turn
+        best_move = None
+        best_move_nim_sum = 0
+
+        for pile_index in range(pile_count):
+            if piles[pile_index] != problematic_state[pile_index]: # piles[pile_index] will ALWAYS be greater than problematic_state[pile_index] since the latter is a later game state
+                difference = piles[pile_index] - problematic_state[pile_index]
+                reduction = difference - 1
+                temp_piles = piles.copy()
+                temp_piles[pile_index] -= reduction
+
+                if frozenset(temp_piles) not in blacklist:
+                    temp_nim_sum = 0
+                    for pile in temp_piles:
+                        temp_nim_sum ^= pile
+
+                    if best_move_nim_sum < temp_nim_sum:
+                        best_move = (pile_index, reduction)
+                        best_move_nim_sum = temp_nim_sum
+                    
+        if best_move is not None:
+            print(temp_piles)
+            return(best_move)
+
+        # Otherwise do some arbitrary move that doesn't lead to a blacklisted state
+        for pile_index in non_empty_pile_indexes:
+            for reduction in range(1, piles[pile_index] + 1):
+                temp_piles = piles.copy()
+                temp_piles[pile_index] -= reduction
+                if frozenset(temp_piles) not in blacklist:
+                    return(reduction, pile_index)
 
 # Main function:
 def main():
